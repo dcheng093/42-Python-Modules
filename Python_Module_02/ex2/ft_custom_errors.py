2c81f81ac1aad5cc8a388b95b856b836c815e444
Daniel Cheng <dcheng@u81z04s04.42kl.edu.my>
2025-12-28 16:42:40 +0800

    stuff

--- Python_Module_02/ex2/ft_custom_errors.py
+++ Python_Module_02/ex2/ft_custom_errors.py
@@ -1,2 +1,18 @@
 #!/usr/bin/python3.10
 
+class GardenError(Exception):
+    def __init__(self, message: str):
+        super().__init__("Caught a garden error: " + message)
+
+
+class PlantError(GardenError):
+    def __init__(self, plant: str, message: str):
+        super().__init__("Caught PlantError: " + message)
+
+
+class WaterError(GardenError):
+    def __init__(self, water: str, message: str):
+        super().__init__("Caught WaterError: " + message)
+
+
+def plant_error()

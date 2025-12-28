20ae59dc9464179f5b25ffd382b0d570fed659bb
Daniel Cheng <dcheng@u81z04s04.42kl.edu.my>
2025-12-28 20:24:22 +0800

    ex4

--- Python_Module_02/ex4/ft_raise_errors.py
+++ Python_Module_02/ex4/ft_raise_errors.py
@@ -6,21 +6,23 @@ def check_plant_health(plant_name: str, water_level: int, sunlight_hours: int):
     elif water_level < 1 or water_level > 10:
         raise ValueError(
             f"Error: Water level {water_level} is too "
-            f"low (min 1)\n" if water_level < 1 else "high (max 10)\n"
+            f"{'low (min 1)' if water_level < 1 else 'high (max 10)'}\n"
         )
+
     elif sunlight_hours < 2 or sunlight_hours > 12:
         raise ValueError(
             f"Error: Sunlight hours {sunlight_hours} is too "
-            f"low (min 2)\n" if sunlight_hours < 2 else "high (max 12)\n"
+            f"{'low (min 2)' if sunlight_hours < 2 else 'high (max 12)'}\n"
         )
-    print(f"Plant: '{plant_name}' is healthy!\n")
+    else:
+        print(f"Plant: '{plant_name}' is healthy!\n")
 
 
 def test_plant_checks():
     print("=== Garden Health Checker ===\n")
     print("Testing good values...")
     try:
-        check_plant_health("tomato", 5, 5)
+        check_plant_health("tomato", 6, 7)
     except ValueError as e:
         print(e)
     print("Testing empty plant name...")
@@ -38,7 +40,7 @@ def test_plant_checks():
         check_plant_health("tomato", 5, 0)
     except ValueError as e:
         print(e)
-    print("=== All error raising tests completed! ===")
+    print("All error raising tests completed!")
 
 
 test_plant_checks()

7bbecf006ce7a40317585662a6fee57937cddabd
Daniel Cheng <dcheng@u81z04s04.42kl.edu.my>
2025-12-30 23:05:47 +0800

    ex3

---
+++ Python_Module_03/ex3/ft_achievement_tracker.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3.10
+
+def achievement_tracker(player):
+    achievments = {'boss_slayer', 'collector', 'first_kill', 'level_10',
+                   'perfectionist', 'speed_demon', 'treasure_hunter'}
+    print("=== Achievement Tracker System ===\n")
+    print(f"Player {player} achievements: {}")

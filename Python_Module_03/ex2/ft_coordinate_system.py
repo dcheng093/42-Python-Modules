b2afe9cd1e029ef8c5861bcae5ba4dcbaaaccd31
Daniel Cheng <dcheng@u80z06s01.42kl.edu.my>
2025-12-30 17:28:32 +0800

    put brackets in output

--- Python_Module_03/ex2/ft_coordinate_system.py
+++ Python_Module_03/ex2/ft_coordinate_system.py
@@ -11,7 +11,7 @@ def distance_between(pos1, pos2):
 
 def parse_coords(coord_str):
     try:
-        x_str, y_str, z_str = coord_str.split(",")
+        x_str, y_str, z_str = coord_str.split(',')
         return (int(x_str), int(y_str), int(z_str))
     except Exception as e:
         print(f"Error parsing coordinates: {e}")
@@ -35,7 +35,7 @@ if __name__ == "__main__":
     parsed = "3,4,0"
     print(f'Parsing coordinates: "{parsed}"')
     print(f"Parsed position: {parse_coords(parsed)}")
-    print(f"Distance between {origin} and {parsed}: "
+    print(f"Distance between {origin} and {parse_coords(parsed)}: "
           f"{distance_between(origin, parse_coords(parsed))}\n")
     invalid = "abc,def,ghi"
     print(f'Parsing invalid coordinates: "{invalid}"')
